7fa3ec5653b4fb5455db1f71adbfabbe5d19009c
Diletta Calussi <s0559842@htw-berlin.de>
2018-11-07 17:26:16 +0100

    removed setup from Prediction

--- excercise_1/catkin_ws/src/prediction/src/Prediction.py
+++ excercise_1/catkin_ws/src/prediction/src/Prediction.py
@@ -22,31 +22,6 @@ class Prediction:
         #Test
         print(self.model.summary())
 
-    def setup_model(self):
-        #define model structure as in mnist_cnn_modified
-        model = Sequential()
-        model.add(Conv2D(filters=32,
-                         kernel_size=(3, 3),
-                         activation='relu',
-                         input_shape=input_shape))
-        model.add(Conv2D(filters=64,
-                         kernel_size=(3, 3),
-                         activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(rate=0.25))
-        model.add(Flatten())
-        model.add(Dense(units=128,
-                        activation='relu'))
-        model.add(Dropout(rate=0.5))
-        model.add(Dense(units=num_classes,
-                        activation='softmax'))
-
-        model.compile(loss=keras.losses.categorical_crossentropy,
-                      optimizer=keras.optimizers.Adadelta(),
-                      metrics=['accuracy'])
-        return model
-
-
 
 def main():
     try:
